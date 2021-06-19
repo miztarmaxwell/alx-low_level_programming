41ff254e06f51c6e210e86c2154c278ecf984275
maxwell <maxwellumeokezue@gmail.com>
2021-06-18 23:03:42 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
-printf("Last digit of %d is %d and is 0\n" n, lastn);
+printf("Last digit of %d is %d and is 0\n", n, lastn);
 }
 else if (lastn < 6 && lastn != 0)
 {
