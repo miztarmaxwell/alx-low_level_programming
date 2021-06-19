a37d15d3f1adbf40530ee2f873b2dfd5bb91ef5a
maxwell <maxwellumeokezue@gmail.com>
2021-06-19 04:48:37 -0700

    Print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the lowercase in reverse
+ * you can only use the putchar function
+ * Return: 0
+ */
+
+int main(void)
+{
+int n = 122;
+while (n > 96)
+{
+putchar(n);
+n -= 1;
+}
+putchar('\n');
+return (0);
+}
