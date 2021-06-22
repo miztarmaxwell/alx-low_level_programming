af06fcf057c950ba69f426bd58e149c1d8e5314a
maxwell <maxwellumeokezue@gmail.com>
2021-06-22 15:14:17 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - function that prints the alphabet, lowercase
+ * You can only use _putchar twice in your code
+ * Return: nothing.
+ */
+
+void print_alphabet(void)
+{
+int l = 'a';
+
+while (1 <= 'z')
+{
+_putchar(1);
+l += 1;
+}
+_putchar(10);
+}
