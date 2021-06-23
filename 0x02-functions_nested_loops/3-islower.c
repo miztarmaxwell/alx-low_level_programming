68838ba543e38cd7df9aac030511a37da369d38c
maxwell <maxwellumeokezue@gmail.com>
2021-06-22 17:20:47 -0700

    Islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _islower _ funtion that checks for lowercase character.
+ * @c: The input character to check
+ * Return: int.
+ */
+
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
+}
