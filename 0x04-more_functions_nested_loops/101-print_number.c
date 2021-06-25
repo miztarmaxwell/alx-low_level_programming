3f11c672b2a3b5688d9d8f0ff832692a82b5a41c
maxwell <maxwellumeokezue@gmail.com>
2021-06-24 18:16:23 -0700

    Print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,7 +11,6 @@ void print_number(int n)
 unsigned int num, num2;
 int i;
 int aux = 1;
-
 if (n < 0)
 {
 n = n * -1;
@@ -23,12 +22,11 @@ if (num > 9)
 {
 while (num >= 10)
 {
-aux = aux * 10; 
+aux = aux * 10;
 num = num / 10;
 }
 _putchar((num2 / aux) + '0');
 aux = aux / 10;
-
 for (i = aux; i >= 1; i = i / 10)
 _putchar((num2 / i) % 10 + '0');
 }
