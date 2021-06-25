d2a4eb8ac566bd85817c8bb3b077d5e20a58d047
maxwell <maxwellumeokezue@gmail.com>
2021-06-25 01:01:19 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,14 @@
  * Return: nothing.
  */
 
-void print_most_numbers(void)  
+void print_most_numbers(void)
 {
 int n = 0;
-
 while (n < 10)
 {
 if (n != 2 && n != 4)
 _putchar(n + '0');
 n += 1;
 }
-_putchar('\n');  
+ _putchar('\n');
 }
