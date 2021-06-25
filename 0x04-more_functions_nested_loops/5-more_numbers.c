5bfc7b04d0c39817e31ff61f6524bb8d5797a2e4
maxwell <maxwellumeokezue@gmail.com>
2021-06-25 01:07:56 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,14 +2,12 @@
 
 /**
  * more_numbers -  function that prints 10 times the numbers, from 0 to 14.
- *
  * Return: nothing.
  */
 
-void more_numbers(void)  
+void more_numbers(void)
 {
 int x, y;
-
 for (x = 0; x < 10; x++)
 {
 for (y = 0; y <= 14; y++)
