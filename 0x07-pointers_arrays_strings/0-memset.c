906cf6285c3ac3e0983f66773d1e13d346f37bda
maxwell <maxwellumeokezue@gmail.com>
2021-07-06 08:53:58 -0700

    Memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,11 +11,13 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i = 0;
-while (i < n)
-{
-*(s + 1) = b;
-i += 1;
-}
-return (s);
+  unsigned int i = 0;
+
+  while (i < n)
+    {
+
+      *(s + i) = b;
+      i += 1;
+    }
+  return (s);
 }
