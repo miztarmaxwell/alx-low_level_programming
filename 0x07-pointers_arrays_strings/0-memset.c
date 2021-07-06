3a61f184cf37fd1c31e665383ae6e7918a1bfaa8
maxwell <maxwellumeokezue@gmail.com>
2021-07-06 09:09:02 -0700

    Memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - function that fills memory with a constant byte.
  *
- * @s: memory area to return
+ * @s: mamory area to return
  * @b: constant byte
  * @n: size of bytes
  * Return: char
@@ -14,9 +14,10 @@ char *_memset(char *s, char b, unsigned int n)
   unsigned int i = 0;
 
   while (i < n)
-    {
-      *(s + i) = b;
-      i += 1;
-    }
+  {
+
+    *(s + i) = b;
+    i += 1;
+  }
   return (s);
 }
